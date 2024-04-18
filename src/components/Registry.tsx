b92dc86204f0ece32dfdfe425b7fe42dbd6a9410
jacob-dt <107429249+jacob-dt@users.noreply.github.com>
2024-04-17 22:55:26 -0400

    initialize Project

---
+++ src/components/Registry.tsx
@@ -0,0 +1,9 @@
+import SectionCreationForm from "./forms/SectionCreationForm";
+
+export default function Registry() {
+    return (
+        <div>
+            <SectionCreationForm />
+        </div>
+    );
+}
---
+++ src/components/forms/SectionCreationForm.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { FormEvent } from "react";
+
+export default function SectionCreationForm() {
+    function newSectionHandler(event: FormEvent) {
+        event.preventDefault();
+        const sectionInput = (event.target as HTMLFormElement).querySelector(
+            "input"
+        );
+        const sectionName = sectionInput?.value;
+        alert("new section: " + sectionName);
+    }
+
+    return (
+        <form onSubmit={newSectionHandler} className="max-w-xs">
+            <label className="block mb-2">
+                <span className="block">Section Name:</span>
+                <input type="text" placeholder="New Section Name" />
+            </label>
+            <button type="submit" className="w-full block">
+                Add Section
+            </button>
+        </form>
+    );
+}
