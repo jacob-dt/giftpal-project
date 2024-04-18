711cd5dad7c462b1fbc0e3d10ab02a2573c05f64
jacob-dt <107429249+jacob-dt@users.noreply.github.com>
2024-04-18 16:46:19 -0400

    add styling

--- src/components/MainHeading.tsx
+++ src/components/MainHeading.tsx
@@ -3,6 +3,8 @@ import { getServerSession } from "next-auth";
 import SignOutButton from "./SignOutButton";
 import SignInButton from "./SignInButton";
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGift } from "@fortawesome/free-solid-svg-icons";
 
 export default async function MainHeading() {
     const sesh = await getServerSession(authorizationOptions);
@@ -10,13 +12,14 @@ export default async function MainHeading() {
     return (
         <header className="p-7 bg-zinc-800 ">
             <div className="flex items-center justify-between ">
-                <Link href="/" className="logo">
-                    GiftPal
+                <Link href="/" className="logo flex items-center">
+                    <FontAwesomeIcon icon={faGift} className="h-6 pr-2" />
+                    <div className="font-bold text-2xl">GiftPal</div>
                 </Link>
                 <div>
                     {sesh && (
                         <>
-                            Signed In As: {sesh?.user?.name}
+                            Username: {sesh?.user?.name}
                             <SignOutButton />
                         </>
                     )}
