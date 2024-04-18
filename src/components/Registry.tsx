adc8b2084e543ede86241000daeadf9725b036eb
jacob-dt <107429249+jacob-dt@users.noreply.github.com>
2024-04-18 10:51:23 -0400

    add registry rename functionality

--- src/components/Registry.tsx
+++ src/components/Registry.tsx
@@ -7,8 +7,31 @@ import { ClientSideSuspense } from "@liveblocks/react";
 import Sections from "./Sections";
 import { RoomProvider } from "@/app/liveblocks.config";
 import Link from "next/link";
+import { FormEvent, useState } from "react";
+import { updateRegistry } from "@/app/actions/registryActions";
+import { metadata } from "@/app/layout";
+import { useRouter } from "next/navigation";
 
 export default function Registry({ id, name }: { id: string; name: string }) {
+    const [registryTitleRenameMode, setRegistryTitleRenameMode] =
+        useState(false);
+    const router = useRouter();
+    async function registryTitleSubmitHandler(event: FormEvent) {
+        event.preventDefault();
+        const registryTitleInput = (
+            event.target as HTMLFormElement
+        ).querySelector("input");
+        if (registryTitleInput) {
+            const newRegistryTitleName = registryTitleInput.value;
+            await updateRegistry(id, {
+                metadata: { registryName: newRegistryTitleName },
+            });
+            registryTitleInput.value = "";
+            setRegistryTitleRenameMode(false);
+            router.refresh();
+        }
+    }
+
     return (
         <RoomProvider
             id={id}
@@ -18,11 +41,26 @@ export default function Registry({ id, name }: { id: string; name: string }) {
                 gifts: new LiveList(),
             }}
         >
-            <ClientSideSuspense fallback={<div>loading...</div>}>
+            <ClientSideSuspense fallback={<div>pending</div>}>
                 {() => (
                     <>
                         <div className="flex gap-2 items-center mb-5">
-                            <h1 className="text-2xl">{name}</h1>
+                            {!registryTitleRenameMode && (
+                                <h1
+                                    onClick={() =>
+                                        setRegistryTitleRenameMode(true)
+                                    }
+                                    className="text-2xl"
+                                >
+                                    {name}
+                                </h1>
+                            )}
+                            {registryTitleRenameMode && (
+                                <form onSubmit={registryTitleSubmitHandler}>
+                                    <input type="text" defaultValue={name} />
+                                </form>
+                            )}
+
                             <Link href={`/registries/${id}/settings`}>
                                 <FontAwesomeIcon icon={faGear} />
                             </Link>
